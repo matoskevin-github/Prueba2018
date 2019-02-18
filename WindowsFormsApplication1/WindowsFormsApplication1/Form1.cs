c1d8bcdaa5f353dbf197663b0090564ab82f09f8
Victor Kevin Matos Secce <45744592+matoskevin-github@users.noreply.github.com>
2019-02-18 01:10:04 -0500

    Add files via upload
    Conexion a Oracle  con Conexion via TNS y Servicio

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.OracleClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*   try
+               {
+                   string oradb = ConfigurationManager.ConnectionStrings["miConexion"].ConnectionString.ToString();
+                   OracleConnection conn = new OracleConnection();
+                   conn.ConnectionString = oradb;
+                   conn.Open();
+                   MessageBox.Show(conn.State.ToString());
+               }
+               catch (Exception ex)
+               {
+                   MessageBox.Show(ex.Message);
+               }*/
+
+
+            string conexionString = "User id = SYSTEM; Password=Peru123.;Data Source = db11g;";
+
+    
+            OracleConnection conexion = new OracleConnection(conexionString);
+            conexion.Open();
+            MessageBox.Show(conexion.State.ToString());
+
+        }
+    }
+}
